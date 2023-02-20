4941a20f666b140d72d8068f2bd932ed51797ce5
eduvela <604-eduvela@users.noreply.gitlab.inf.uva.es>
2023-02-20 20:32:14 +0100

    Fixed scroll mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,14 +60,6 @@ $(document).ready(function () {
         });
     });
 
-    // smooth scrolling
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        }, 500, 'linear')
-    });
-
     //Type effect
     var typed = new Typed(".typing-text", {
         strings: ["full-stack development", "photo editing", "branding", "logo creation"],
@@ -81,16 +73,14 @@ $(document).ready(function () {
         let id = $(this).attr("id").replace("scroll-to-", "");
         $('html, body').animate({
             scrollTop: $("#" + id).offset().top - $("header").outerHeight()
-        }, 1000);
+        }, 100);
     });
 
     $("[id^='scroll-top']").click(function () {
         $('html, body').animate({
             scrollTop: $("#home").offset().top - $("header").outerHeight()
-        }, 1000);
+        }, 100);
     });
-
-
 
     //To get portfolio data
     function fetchData(type) {
@@ -230,51 +220,3 @@ document.addEventListener('keydown', function (e) {
         e.preventDefault();
     }
 });
-
-/* ===== SCROLL REVEAL ANIMATION ===== */
-const srtop = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 100,
-    reset: true
-});
-
-/* SCROLL HOME */
-srtop.reveal('.home .content h3', { delay: 200 });
-srtop.reveal('.home .content p', { delay: 200 });
-srtop.reveal('.home .content .btn', { delay: 200 });
-
-srtop.reveal('.home .image', { delay: 400 });
-srtop.reveal('.home .linkedin', { interval: 600 });
-srtop.reveal('.home .github', { interval: 800 });
-srtop.reveal('.home .twitter', { interval: 1000 });
-srtop.reveal('.home .telegram', { interval: 600 });
-srtop.reveal('.home .instagram', { interval: 600 });
-srtop.reveal('.home .dev', { interval: 600 });
-
-/* SCROLL ABOUT */
-srtop.reveal('.about .content h3', { delay: 200 });
-srtop.reveal('.about .content .tag', { delay: 200 });
-srtop.reveal('.about .content p', { delay: 200 });
-srtop.reveal('.about .content .box-container', { delay: 200 });
-srtop.reveal('.about .content .resumebtn', { delay: 200 });
-
-
-/* SCROLL SKILLS */
-srtop.reveal('.skills .container', { interval: 200 });
-srtop.reveal('.skills .container .bar', { delay: 400 });
-
-/* SCROLL EDUCATION */
-srtop.reveal('.education .box', { interval: 200 });
-
-/* SCROLL PROJECTS */
-srtop.reveal('.projects .box', { interval: 200 });
-
-/* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
-
-/* SCROLL CONTACT */
-srtop.reveal('.contact .container', { delay: 400 });
-srtop.reveal('.contact .container .form-group', { delay: 400 });
-
