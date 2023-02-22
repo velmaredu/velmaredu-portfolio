81edfc6e25a84718746dfde981cd0bfbcc6133e5
eduvela <604-eduvela@users.noreply.gitlab.inf.uva.es>
2023-02-22 21:40:02 +0100

    Experience dates fixed

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -99,7 +99,7 @@ function showExperience(experience, num) {
               <div class="desc"> 
                 <h3>${experience.role}</h3> 
                 <p id="time-block"> 
-                  <span id="time-text">${experience.time}</span> · 
+                  <span id="time-text">${experience.time}</span>
                   <span id="time-diff"></span> 
                 </p> 
               </div> 
@@ -129,3 +129,34 @@ document.addEventListener('keydown', function (e) {
         e.preventDefault();
     }
 });
+
+$(window).on('load', function() {
+    let timeBlocksElements = document.querySelectorAll("#time-block");
+    timeBlocksElements.forEach(function (timeBlockElement) {
+        let timeTextElement = timeBlockElement.querySelector("#time-text");
+        let { specificDate, currentDate } = setDatesFromTimeText(timeTextElement.innerHTML);
+        let diffYears = currentDate.diff(specificDate, 'years');
+        let diffMonths = currentDate.subtract(diffYears, 'years').diff(specificDate, 'months') + 1;
+        let diffString = " · ";
+        if (diffYears > 0) {
+            diffString += diffYears + " years ";
+        }
+        if (diffMonths > 0) {
+            diffString += diffMonths + " months";
+        }
+        let timeDiffElement = timeBlockElement.querySelector("#time-diff");
+        timeDiffElement.innerHTML = diffString;
+    });
+});
+
+function setDatesFromTimeText(timeText) {
+    let dateRegex = /\w{3}\. \d{4}|present/g;
+    let dates = timeText.match(dateRegex);
+    if (dates && dates.length === 2) {
+        let specificDate = moment(dates[0], "MMM. YYYY");
+        let currentDate = (dates[1] === 'present') ? moment() : moment(dates[1], "MMM. YYYY");
+        return { specificDate, currentDate };
+    } else {
+        console.log("The dates could not be found in the time-text element.");
+    }
+}
--- experience/experience.js
+++ experience/experience.js
@@ -1,34 +1,3 @@
-window.onload = function () {
-    let timeBlocksElements = document.querySelectorAll("#time-block");
-    timeBlocksElements.forEach(function (timeBlockElement) {
-        let timeTextElement = timeBlockElement.querySelector("#time-text");
-        let { specificDate, currentDate } = setDatesFromTimeText(timeTextElement.innerHTML);
-        let diffYears = currentDate.diff(specificDate, 'years');
-        let diffMonths = currentDate.subtract(diffYears, 'years').diff(specificDate, 'months') + 1;
-        let diffString = "";
-        if (diffYears > 0) {
-            diffString += diffYears + " years ";
-        }
-        if (diffMonths > 0) {
-            diffString += diffMonths + " months";
-        }
-        let timeDiffElement = timeBlockElement.querySelector("#time-diff");
-        timeDiffElement.innerHTML = diffString;
-    });
-}
-
-function setDatesFromTimeText(timeText) {
-    let dateRegex = /\w{3}\. \d{4}|present/g;
-    let dates = timeText.match(dateRegex);
-    if (dates && dates.length === 2) {
-        let specificDate = moment(dates[0], "MMM. YYYY");
-        let currentDate = (dates[1] === 'present') ? moment() : moment(dates[1], "MMM. YYYY");
-        return { specificDate, currentDate };
-    } else {
-        console.log("The dates could not be found in the time-text element.");
-    }
-}
-
 $(document).ready(function () {
     $("[id^='scroll-top']").click(function () {
         $('html, body').animate({
