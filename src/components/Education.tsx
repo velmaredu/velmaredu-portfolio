162285d49025c33d5759fe129b8b10dec3343b69
Eduardo Velasco <velmaredu@gmail.com>
2023-12-01 22:17:47 +0100

    Education and styles

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,43 +1,42 @@
+import { Card, CardActionArea, Stack, Typography } from "@mui/material"
+import "../assets/styles/education.scss"
+import educationData from "../data/education.json"
+import { School } from "@mui/icons-material"
+
 function Education() {
+
+    const renderEducations = () => {
+        return educationData.map((education, index) => (
+            <Card key={index} className="box">
+                <CardActionArea href={education.link}>
+                    <Stack direction={"row"} className="box">
+                        <img
+                            draggable="false"
+                            src={education.logo}
+                            alt=""
+                        />
+                        <Stack className="content">
+                            <Typography variant="h5">{education.title}</Typography>
+                            <p>{education.place}</p>
+                            <Typography variant="h6">{education.period}</Typography>
+                        </Stack>
+                    </Stack>
+                </CardActionArea>
+            </Card>
+        ))
+    }
+
     return (
-        <section className="education" id="education">
-            <h1 className="heading">
-                <i className="fas fa-graduation-cap"></i> My <span>Education</span>
-            </h1>
+        <Stack className="section" id="education">
+            <Typography variant="h2" className="section-title">
+                <School fontSize="large" /> My <span>Education</span>
+            </Typography>
 
-            <div className="container">
-                <a href="https://www.uva.es/">
-                    <div className="box">
-                        <img
-                            draggable="false"
-                            src="./assets/images/education/logo-uva.svg"
-                            alt=""
-                        />
-                        <div className="content">
-                            <h3>Bachelor of Computer Engineering</h3>
-                            <p>University of Valladolid | UVa</p>
-                            <h4>sep 2018 - jan 2023</h4>
-                        </div>
-                    </div>
-                </a>
-
-                <a href="https://www.lasalle.es/">
-                    <div className="box">
-                        <img
-                            draggable="false"
-                            src="./assets/images/education/la-salle-logo.jpg"
-                            alt=""
-                        />
-                        <div className="content">
-                            <h3>Science High School Degree</h3>
-                            <p>La Salle</p>
-                            <h4>sep 2016 - jun 2018</h4>
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </section>
+            <Stack className="container">
+                {renderEducations()}
+            </Stack>
+        </Stack>
     )
 }
 
-export default Education;
+export default Education
